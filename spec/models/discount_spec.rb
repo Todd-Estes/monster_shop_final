4341b564216ead7e461c04ad179b5d9f6e7c470a
todd-estes <elestes@gmail.com>
2020-11-07 12:12:09 -0700

    Create Discount model class and pass Discount model spec

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Discount do
   describe 'Relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :minimum_qty}
+    it {should validate_presence_of :percent_off}
+  end
 end
