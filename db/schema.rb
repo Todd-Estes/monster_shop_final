5830a53f0ac1113250a1232a48f35b2e50b22280
todd-estes <elestes@gmail.com>
2020-11-07 11:57:32 -0700

    Create Discounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_161751) do
+ActiveRecord::Schema.define(version: 2020_11_07_185259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "name"
+    t.integer "minimum_qty"
+    t.float "percent_off"
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_discounts_on_merchant_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -84,6 +92,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_161751) do
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
+  add_foreign_key "discounts", "merchants"
   add_foreign_key "items", "merchants"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
