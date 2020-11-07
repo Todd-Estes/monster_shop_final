ed69114451473931600c0bdef5ac128f7e5a923c
todd-estes <elestes@gmail.com>
2020-11-07 10:28:51 -0700

    Change coupons to discounts within discounts index spec

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -16,8 +16,8 @@ describe 'As a merchant user' do
                                               password: 'password')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_user)
 
-    @coupon_1 = @merchant_1.coupons.create!(name: "Summer Deal 50%-Off", code: "50OFF", percent_off: 50)
-    @coupon_2 = @merchant_1.coupons.create!(name: "Holiday Weekend 75%-Off", code: "75OFF", percent_off: 75, enabled: false)
+    @discount_1 = @merchant_1.discounts.create!(name: "Summer Deal 50%-Off", code: "50OFF", percent_off: 50)
+    @discount_2 = @merchant_1.discounts.create!(name: "Holiday Weekend 75%-Off", code: "75OFF", percent_off: 75, enabled: false)
 
     visit '/'
     click_on 'Login'
@@ -28,43 +28,43 @@ describe 'As a merchant user' do
     click_button 'Login'
   end
 
-  describe "when I visit my coupons index view" do
+  describe "when I visit my discounts index view" do
     before(:each) do
       visit '/merchant'
 
-      click_on 'Manage Coupons'
+      click_on 'Manage Discounts'
   end
 
   it 'can see the name, code, percent_off, and enabled status' do
-    within("#coupon-#{@coupon_1.id}") do
-      expect(page).to have_link("#{coupon_1.name}")
-      expect(page).to content("#{coupon_1.code}")
-      expect(page).to content("#{coupon_1.percent_off}%")
-      expect(page).to content("#{coupon_1.status}")
+    within("#discount-#{@discount_1.id}") do
+      expect(page).to have_link("#{discount_1.name}")
+      expect(page).to content("#{discount_1.code}")
+      expect(page).to content("#{discount_1.percent_off}%")
+      expect(page).to content("#{discount_1.status}")
     end
 
-      within("#coupon-#{@coupon_1.id}") do
-        expect(page).to have_link("#{coupon_2.name}")
-        expect(page).to content("#{coupon_2.code}")
-        expect(page).to content("#{coupon_2.percent_off}%")
-        expect(page).to content("#{coupon_2.status}")
+      within("#discount-#{@discount_1.id}") do
+        expect(page).to have_link("#{discount_2.name}")
+        expect(page).to content("#{discount_2.code}")
+        expect(page).to content("#{discount_2.percent_off}%")
+        expect(page).to content("#{discount_2.status}")
       end
    end
 
-   it "can click a coupon name and be linked to coupon show page" do
-      click_link("#{coupon_1.name}")
+   it "can click a discount name and be linked to discount show page" do
+      click_link("#{discount_1.name}")
 
-      expect(current_path).to eq("merchant/#{coupon_1.id}")
+      expect(current_path).to eq("merchant/#{discount_1.id}")
    end
  end
 end
 
-   # it "can see a delete button next to each coupon" do
-   #   within("#coupon-#{@coupon_1.id}") do
-   #     expect(page).to have_content("Delete Coupon")
+   # it "can see a delete button next to each discount" do
+   #   within("#discount-#{@discount_1.id}") do
+   #     expect(page).to have_content("Delete Discount")
    #   end
    #
-   #    within("#coupon-#{@coupon_1.id}") do
-   #      expect(page).to have_content("Delete Coupon")
+   #    within("#discount-#{@discount_1.id}") do
+   #      expect(page).to have_content("Delete Discount")
    #    end
    # end
