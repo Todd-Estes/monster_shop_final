f054f8ca5b619a5acc43a1592f7ad65623189bb6
todd-estes <elestes@gmail.com>
2020-11-07 10:20:22 -0700

    Change coupons spec folder to discounts folder and change coupons to discounts within edit spec

--- spec/features/discounts/edit_spec.rb
+++ spec/features/discounts/edit_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Coupon Edit Page' do
+describe 'Discount Edit Page' do
   before(:each) do
     @merchant_1 = Merchant.create(name: 'Bobs Exotics',
                                    address: '100 AE ST',
@@ -16,8 +16,8 @@ describe 'Coupon Edit Page' do
                                               password: 'password')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_user)
 
-    @coupon_1 = @merchant_1.coupons.create!(name: "Summer Deal 50%-Off", code: "50OFF", percent_off: 50)
-    @coupon_2 = @merchant_1.coupons.create!(name: "Holiday Weekend 75%-Off", code: "75OFF", percent_off: 75, enabled: false)
+    @discount_1 = @merchant_1.discounts.create!(name: "Summer Deal 50%-Off", code: "50OFF", percent_off: 50)
+    @discount_2 = @merchant_1.discounts.create!(name: "Holiday Weekend 75%-Off", code: "75OFF", percent_off: 75, enabled: false)
 
     visit '/'
     click_on 'Login'
@@ -28,55 +28,55 @@ describe 'Coupon Edit Page' do
     click_button 'Login'
   end
 
-  describe "when I visit my coupons index view" do
+  describe "when I visit my discounts index view" do
     before(:each) do
       visit '/merchant'
 
-      click_on 'Manage Coupons'
+      click_on 'Manage Discounts'
   end
 
   it 'can see an edit link next to each coupon'
-    within("#coupon-#{@coupon_1.id}") do
-      expect(page).to have_link("Edit Coupon")
+    within("#coupon-#{@discount_1.id}") do
+      expect(page).to have_link("Edit Discount")
     end
 
-    within("#coupon-#{@coupon_2.id}") do
-      expect(page).to have_link("Edit Coupon")
+    within("#coupon-#{@discount_2.id}") do
+      expect(page).to have_link("Edit Discount")
     end
   end
 
   it 'can click the edit link and be taken to the edit coupon view' do
-    within("#coupon-#{@coupon_1.id}") do
-      click_link("Edit Coupon")
+    within("#coupon-#{@discount_1.id}") do
+      click_link("Edit Discount")
     end
 
-    expect(current_path).to eq('merchant/coupons/edit')
-    expect(page).to have_content("Edit Coupon")
-    expect(page).to have_field('Name', with: @coupon_1.name)
-    expect(page).to have_field('Code', with: @coupon_1.code)
-    expect(page).to have_field('Percent Off', with: @coupon_1.discount)
+    expect(current_path).to eq('merchant/discounts/edit')
+    expect(page).to have_content("Edit Discount")
+    expect(page).to have_field('Name', with: @discount_1.name)
+    expect(page).to have_field('Code', with: @discount_1.code)
+    expect(page).to have_field('Percent Off', with: @discount_1.discount)
   end
 
   it 'can fill in a field with a valid input and submit changes' do
-    within("#coupon-#{@coupon_1.id}") do
-      click_link("Edit Coupon")
+    within("#coupon-#{@discount_1.id}") do
+      click_link("Edit Discount")
     end
 
-    fill_in 'Name', with: 'Black Friday Coupon'
-    click_on 'Update Coupon'
+    fill_in 'Name', with: 'Black Friday Discount'
+    click_on 'Update Discount'
   end
 
   it 'can submit coupon changes and be redirected to coupon index where it can see edited coupon' do
-    within("#coupon-#{@coupon_1.id}") do
-      click_link("Edit Coupon")
+    within("#coupon-#{@discount_1.id}") do
+      click_link("Edit Discount")
     end
 
-    fill_in 'Name', with: 'Black Friday Coupon'
-    click_on 'Update Coupon'
-    expect(current_path).to eq('/merchant/coupons')
+    fill_in 'Name', with: 'Black Friday Discount'
+    click_on 'Update Discount'
+    expect(current_path).to eq('/merchant/discounts')
 
-    within("#coupon-#{@coupon_1.id}") do
-      expect(page).to have_content('Black Friday Coupon')
+    within("#coupon-#{@discount_1.id}") do
+      expect(page).to have_content('Black Friday Discount')
     end
   end
 end
